931aa592028855aeff9178a52de3e5785b1c104e
nilsw13 <nils.wenting@laplateforme.io>
2025-06-27 01:04:55 +0200

    cleaned

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.variable}  antialiased bg-white` }
+        className={`${inter.variable} overflow-x-hidden  antialiased bg-white` }
       >
 
         <HeaderNav/>
